65ce6f756bbc962c5b532b3e41c6235661ed2944
ssghajar <ssghajar.work@gmail.com>
2025-07-11 13:42:56 +0330

    build: export store types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { SipProvider, useSipProvider } from './provider';
 export { Audio as AudioStream, Video as VideoStream } from './components/index';
+export * from './types';
+export * from './store/types';
