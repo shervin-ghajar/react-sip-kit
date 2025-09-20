ca9b4475beb316d671a8252c81de79271148677e
Shervin Ghajar <ssghajar.work@gmail.com>
2025-09-20 17:46:40 +0330

    v0.5.22

--- dist/hooks/useDeep/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function useDeep<S, U>(selector: (state: S) => U): (state: S) => U;
--- dist/hooks/useSipManager/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { SipAccountConfig } from '../../configs/types';
-import { LineType } from '../../store/types';
-export declare function useSipManager({ username }: {
-    username: SipAccountConfig['username'];
-}): () => {
-    status: import("../../store/types").SipUserAgentStatus | undefined;
-    lines: LineType[];
-};
