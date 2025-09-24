4873e9d18c2d5cde136aeba3fd6ecc8bdb961928
Shervin Ghajar <ssghajar.work@gmail.com>
2025-09-24 18:56:19 +0330

    build: 0.6.01

--- dist/methods/registration/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { SipAccountConfig } from '../../configs/types';
-export declare function register(username: SipAccountConfig['username'], userAgent?: import("../..").SipUserAgent | null): void;
-export declare function unregister(username: SipAccountConfig['username'], skipUnsubscribe?: boolean, userAgent?: import("../..").SipUserAgent | null): void;
-export declare function refreshRegistration(username: SipAccountConfig['username']): void;
