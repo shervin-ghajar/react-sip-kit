5cb4301a5e77e6298536831e7508e37291f066d9
Shervin Ghajar <ssghajar.work@gmail.com>
2025-10-04 17:54:16 +0330

    remove dist

--- dist/methods/registration/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { SipConfigs } from '../../configs/types';
-export declare function register(configKey: SipConfigs['key'], userAgent?: import("../..").SipUserAgent | null): void;
-export declare function unregister(configKey: SipConfigs['key'], skipUnsubscribe?: boolean, userAgent?: import("../..").SipUserAgent | null): void;
-export declare function refreshRegistration(configKey: SipConfigs['key']): void;
